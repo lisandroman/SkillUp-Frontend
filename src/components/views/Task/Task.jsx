5aac61a92df7925f3ff88ed43adfbf34a1451c05
lisandroman <lisandromansilla.dg@gmail.com>
2022-07-15 23:52:53 +0300

    Create Card List - Mobile First

--- src/components/views/Task/Task.jsx
+++ src/components/views/Task/Task.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import { Header } from '../../Header/Header'
+import './Task.styles.css'
 
 export const Task = () => {
   return (
-    // <div className='container'>Task</div>
-    <Header />
+    <>
+      <Header />
+      <main id="tasks">
+        <section className='wrapperList'>
+          <div className="listHeader">
+            <h2>Mis Tareas</h2>
+          </div>
+          <div className="list">
+            <div className="card">
+              <div className="close">x</div>
+              <h3>Tarea 1</h3>
+              <h6>15/07/2022 16.40hs</h6>
+              <h5>Lisandro Mansilla</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Baja</button>
+              <p>Descripción Fake...</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
