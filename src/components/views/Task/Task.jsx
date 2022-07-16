51a709775d75b9cf801bfc6f4c1442000543fac7
lisandroman <lisandromansilla.dg@gmail.com>
2022-07-16 15:34:42 +0300

    Handle Window Resize with useState & useEffect

--- src/components/views/Task/Task.jsx
+++ src/components/views/Task/Task.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../../Header/Header'
 import './Task.styles.css'
 
 export const Task = () => {
+
+  const [isPhone, setIsPhone] = useState(window.innerWidth < 900 ? true : false)
+
+  const handleResize = () => {
+    if(window.innerWidth < 900) setIsPhone(true);
+    else setIsPhone(false);
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  })
+  
+  const limitString = (str) => {
+    if(str.length > 370)
+      return {string: str.slice(0, 250).concat("..."), addButton: true}
+    return {string: str, addButton: false}
+  } 
+
   return (
     <>
       <Header />
@@ -11,131 +31,80 @@ export const Task = () => {
           <div className="listHeader">
             <h2>Mis Tareas</h2>
           </div>
-          <div className="groupList">
-
-            <div className="list">
-              <h4>Nuevas</h4>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
+          {isPhone 
+            ? (
+              <div className="list phone">
+                <div className="card">
+                  <div className="close">x</div>
+                  <h3>Tarea 1</h3>
+                  <h6>15/07/2022 16.40hs</h6>
+                  <h5>Lisandro Mansilla</h5>
+                  <button type="button">Nueva</button>
+                  <button type="button">Baja</button>
+                  <p>Descripción Fake...</p>
+                </div>
               </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
+              )
+            : (
+              <div className="groupList">
+                <div className="list">
+                  <h4>Nuevas</h4>
+                  <div className="card">
+                    <div className="close">x</div>
+                    <h3>Tarea 1</h3>
+                    <h6>15/07/2022 16.40hs</h6>
+                    <h5>Lisandro Mansilla</h5>
+                    <button type="button">Nueva</button>
+                    <button type="button">Baja</button>
+                    <p>{limitString(`
+                      Contrary to popular belief, Lorem Ipsum is not simply random text.
+                      It has roots in a piece of classical Latin literature from 45 BC,
+                      making it over 2000 years old. Richard McClintock, a Latin professor
+                      at Hampden-Sydney College in Virginia, looked up one of the more obscure
+                      Latin words, consectetur, from a Lorem Ipsum passage, and going through
+                      the cites of the word in classical literature, discovered the 
+                      undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+                      1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good 
+                      and Evil) by Cicero, written in 45 BC. This book is a treatise 
+                      on the theory of ethics, very popular during the Renaissance.
+                      The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", 
+                      comes from a line in section 1.10.32.
+                      The standard chunk of Lorem Ipsum used since the 1500s is reproduced 
+                      below for those interested. Sections 1.10.32 and 1.10.33 from "de
+                      Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+                      exact original form, accompanied by English versions from the 1914 
+                      translation by H. Rackham.
+                      `).string}
+                    </p>
+                  </div>
+                </div>
+                <div className="list">
+                  <h4>En proceso</h4>
+                  <div className="card">
+                    <div className="close">x</div>
+                    <h3>Tarea 1</h3>
+                    <h6>15/07/2022 16.40hs</h6>
+                    <h5>Lisandro Mansilla</h5>
+                    <button type="button">Nueva</button>
+                    <button type="button">Baja</button>
+                    <p>Descripción Fake...</p>
+                  </div>
+                </div>
+                <div className="list">
+                  <h4>Finalizadas</h4>
+                  <div className="card">
+                    <div className="close">x</div>
+                    <h3>Tarea 1</h3>
+                    <h6>15/07/2022 16.40hs</h6>
+                    <h5>Lisandro Mansilla</h5>
+                    <button type="button">Nueva</button>
+                    <button type="button">Baja</button>
+                    <p>Descripción Fake...</p>
+                  </div>
+                </div>
               </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-            </div>
-            <div className="list">
-              <h4>En proceso</h4>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-            </div>
-            <div className="list">
-              <h4>Finalizadas</h4>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-              <div className="card">
-                <div className="close">x</div>
-                <h3>Tarea 1</h3>
-                <h6>15/07/2022 16.40hs</h6>
-                <h5>Lisandro Mansilla</h5>
-                <button type="button">Nueva</button>
-                <button type="button">Baja</button>
-                <p>Descripción Fake...</p>
-              </div>
-            </div>
-
-          </div>
-
-          <div className="list phone">
-            <div className="card">
-              <div className="close">x</div>
-              <h3>Tarea 1</h3>
-              <h6>15/07/2022 16.40hs</h6>
-              <h5>Lisandro Mansilla</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Baja</button>
-              <p>Descripción Fake...</p>
-            </div>
-            <div className="card">
-              <div className="close">x</div>
-              <h3>Tarea 1</h3>
-              <h6>15/07/2022 16.40hs</h6>
-              <h5>Lisandro Mansilla</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Baja</button>
-              <p>Descripción Fake...</p>
-            </div>
-            <div className="card">
-              <div className="close">x</div>
-              <h3>Tarea 1</h3>
-              <h6>15/07/2022 16.40hs</h6>
-              <h5>Lisandro Mansilla</h5>
-              <button type="button">Nueva</button>
-              <button type="button">Baja</button>
-              <p>Descripción Fake...</p>
-            </div>
-          </div>
-
+              )
+          }
         </section>
       </main>
     </>
