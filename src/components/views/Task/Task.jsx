9cc5f68d2229fd58901c28f2907ca6b3019a740a
lisandroman <lisandromansilla.dg@gmail.com>
2022-07-16 15:05:53 +0300

    Desktop Cards View

--- src/components/views/Task/Task.jsx
+++ src/components/views/Task/Task.jsx
@@ -11,7 +11,120 @@ export const Task = () => {
           <div className="listHeader">
             <h2>Mis Tareas</h2>
           </div>
-          <div className="list">
+          <div className="groupList">
+
+            <div className="list">
+              <h4>Nuevas</h4>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+            </div>
+            <div className="list">
+              <h4>En proceso</h4>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+            </div>
+            <div className="list">
+              <h4>Finalizadas</h4>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+              <div className="card">
+                <div className="close">x</div>
+                <h3>Tarea 1</h3>
+                <h6>15/07/2022 16.40hs</h6>
+                <h5>Lisandro Mansilla</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Baja</button>
+                <p>Descripción Fake...</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="list phone">
+            <div className="card">
+              <div className="close">x</div>
+              <h3>Tarea 1</h3>
+              <h6>15/07/2022 16.40hs</h6>
+              <h5>Lisandro Mansilla</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Baja</button>
+              <p>Descripción Fake...</p>
+            </div>
+            <div className="card">
+              <div className="close">x</div>
+              <h3>Tarea 1</h3>
+              <h6>15/07/2022 16.40hs</h6>
+              <h5>Lisandro Mansilla</h5>
+              <button type="button">Nueva</button>
+              <button type="button">Baja</button>
+              <p>Descripción Fake...</p>
+            </div>
             <div className="card">
               <div className="close">x</div>
               <h3>Tarea 1</h3>
@@ -22,6 +135,7 @@ export const Task = () => {
               <p>Descripción Fake...</p>
             </div>
           </div>
+
         </section>
       </main>
     </>
