e335a423a7996c6a6a5758f3932304fb09a495d2
lisandroman <lisandromansilla.dg@gmail.com>
2022-07-28 17:59:50 +0300

    Refactoring Card List with API Data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export const Card = ({ card: { title, datetime, creator, description, type, priority}}) => {
+export const Card = ({ card: { title, createdAt, status, user:{userName}, description, type, importance}}) => {
   return (
     <div>
       <div className="card">
         <div className="close">x</div>
         <h3>{title}</h3>
-        <h6>{datetime}</h6>
-        <h5>{creator}</h5>
-        <button type="button">{type}</button>
-        <button type="button">{priority}</button>
+        <h6>{createdAt}</h6>
+        <h5>{userName}</h5>
+        <button type="button">{status}</button>
+        <button type="button">{importance}</button>
         <p>{description}</p>
       </div>
     </div>
--- src/components/views/Task/Task.jsx
+++ src/components/views/Task/Task.jsx
@@ -1,14 +1,34 @@
 
-import './Task.styles.css'
+import { useEffect, useState } from 'react'
+
 import { useResize } from '../../../hooks/useResize'
 import { Header } from '../../Header/Header'
 import { cardsData } from './data'
 import { Card } from '../../Card/Card'
 import { TaskForm } from '../../TaskForm/TaskForm'
 
+import './Task.styles.css'
+
+const { REACT_APP_API_ENDPOINT } = process.env
+
 export const Task = () => {
 
   const { isPhone } = useResize();
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+
+    fetch(`${REACT_APP_API_ENDPOINT}task`, {
+      headers: { 'Content-Type':'application/json',
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+    })
+    .then(response => response.json())
+    .then(data => {
+      setList(data.result)
+    })
+  }, [])
+  
 
   const limitString = (str) => {
     if (str.length > 370)
@@ -17,7 +37,27 @@ export const Task = () => {
   }
 
   const renderAllCards = () => {
-    return cardsData.map(card =>
+    return list?.map(card =>
+      <Card key={card.id} card={card} />
+    )
+  }
+  const renderNewCards = () => {
+    return list?.filter((data) => data.status === "NEW") 
+    .map(card =>
+      <Card key={card.id} card={card} />
+    )
+  }
+
+  const renderInProgressCards = () => {
+    return list?.filter((data) => data.status === "IN PROGRESS") 
+    .map(card =>
+      <Card key={card.id} card={card} />
+    )
+  }
+
+  const renderFinishedCards = () => {
+    return list?.filter((data) => data.status === "FINISHED") 
+    .map(card =>
       <Card key={card.id} card={card} />
     )
   }
@@ -41,58 +81,16 @@ export const Task = () => {
               <div className="groupList">
                 <div className="list">
                   <h4>Nuevas</h4>
-                  <div className="card">
-                    <div className="close">x</div>
-                    <h3>Tarea 1</h3>
-                    <h6>15/07/2022 16.40hs</h6>
-                    <h5>Lisandro Mansilla</h5>
-                    <button type="button">Nueva</button>
-                    <button type="button">Baja</button>
-                    <p>{limitString(`
-                      Contrary to popular belief, Lorem Ipsum is not simply random text.
-                      It has roots in a piece of classical Latin literature from 45 BC,
-                      making it over 2000 years old. Richard McClintock, a Latin professor
-                      at Hampden-Sydney College in Virginia, looked up one of the more obscure
-                      Latin words, consectetur, from a Lorem Ipsum passage, and going through
-                      the cites of the word in classical literature, discovered the 
-                      undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
-                      1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good 
-                      and Evil) by Cicero, written in 45 BC. This book is a treatise 
-                      on the theory of ethics, very popular during the Renaissance.
-                      The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", 
-                      comes from a line in section 1.10.32.
-                      The standard chunk of Lorem Ipsum used since the 1500s is reproduced 
-                      below for those interested. Sections 1.10.32 and 1.10.33 from "de
-                      Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-                      exact original form, accompanied by English versions from the 1914 
-                      translation by H. Rackham.
-                      `).string}
-                    </p>
-                  </div>
+                  {renderNewCards()}
                 </div>
+
                 <div className="list">
                   <h4>En proceso</h4>
-                  <div className="card">
-                    <div className="close">x</div>
-                    <h3>Tarea 1</h3>
-                    <h6>15/07/2022 16.40hs</h6>
-                    <h5>Lisandro Mansilla</h5>
-                    <button type="button">Nueva</button>
-                    <button type="button">Baja</button>
-                    <p>Descripción Fake...</p>
-                  </div>
+                  {renderInProgressCards()}
                 </div>
                 <div className="list">
                   <h4>Finalizadas</h4>
-                  <div className="card">
-                    <div className="close">x</div>
-                    <h3>Tarea 1</h3>
-                    <h6>15/07/2022 16.40hs</h6>
-                    <h5>Lisandro Mansilla</h5>
-                    <button type="button">Nueva</button>
-                    <button type="button">Baja</button>
-                    <p>Descripción Fake...</p>
-                  </div>
+                  {renderFinishedCards()}
                 </div>
               </div>
             )
